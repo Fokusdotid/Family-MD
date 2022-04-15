2b7efaf9ca5f6113793d7801320304325507793f
Fokus ID <40955113+Fokusdotid@users.noreply.github.com>
2022-04-15 14:45:44 +0700

    Perbaikan

--- plugins/g-promote.js
+++ plugins/g-promote.js
@@ -8,7 +8,7 @@ let handler = async (m, { teks, conn, isOwner, isAdmin, args }) => {
 if(m.quoted.sender === ownerGroup || m.quoted.sender === conn.user.jid) return;
 let usr = m.quoted.sender;
 let nenen = await conn.groupParticipantsUpdate(m.chat, [usr], "promote"); return;
-if (nenen) m.reply('sukses promote @' + user.split('@')[0] + '!');
+if (nenen) m.reply(`sukses promote @${user.split('@')[0]}!`);
 }
   if (!m.mentionedJid[0]) throw `tag yang mau dinaikan jabatannya`;
   let users = m.mentionedJid.filter(
@@ -17,7 +17,7 @@ if (nenen) m.reply('sukses promote @' + user.split('@')[0] + '!');
   for (let user of users)
     if (user.endsWith("@s.whatsapp.net"))
       let anu = await conn.groupParticipantsUpdate(m.chat, [user], "promote");
-      if (anu) m.reply('sukses promote @' + user.split('@')[0] + '!');
+      if (anu) m.reply(`sukses promote @${user.split('@')[0]}!`)
 };
 
 handler.help = ['promote @user']
