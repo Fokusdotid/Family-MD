1b85e56cadcb1f3d1129635fac7235f625ed4ccd
FokusDotId <mr.familia13@gmail.com>
2022-04-22 21:58:23 +0700

    add fake

---
+++ plugins/__allfake.js
@@ -0,0 +1,21 @@
+let fs = require('fs')
+let fetch = require('node-fetch')
+let handler = m => m
+handler.all = async function (m) {
+	let pp = 'https://telegra.ph/file/2d06f0936842064f6b3bb.png'
+	try {
+		pp = await this.profilePictureUrl(m.sender, 'image')
+	} catch (e) {
+	} finally {
+		global.axios = require('axios')
+		global.request = require('require')
+
+		global.users = global.db.data.users[m.sender]
+		global.chats = global.db.data.chats[m.chat]
+		global.ftroli = { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { itemCount: 9999999999999999999999999999999999999999999999999999999, status: 1, surface: 1, message: wm, orderTitle: wm, sellerJid: '0@s.whatsapp.net' } } }
+		global.fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': require('fs').readFileSync('./src/logo.jpg'), thumbnail: require('fs').readFileSync('./src/logo.jpg'),sendEphemeral: true}}}
+		// Tambahin ya
+	}
+}
+
+module.exports = handler
