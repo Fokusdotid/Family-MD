ff8da6c419ebe3427548386ee530f3fc0d534177
Fokus ID <40955113+Fokusdotid@users.noreply.github.com>
2022-04-23 11:39:16 +0700

    update

--- plugins/p-join.js
+++ plugins/p-join.js
@@ -4,13 +4,13 @@ let handler = async (m, { conn, text }) => {
     let [_, code] = text.match(linkRegex) || []
     if (!code) throw 'Link Salah'
     let res = await conn.groupAcceptInvite(code)
-    m.reply(`Berhasil join ke grup ${await conn.getName(res)}`).then(() => {
+    conn.reply(m.chat, `Berhasil join ke grup ${await conn.getName(res)}`, m).then(() => {
         var jumlahHari = 86400000 * 0.5
         var now = new Date() * 1
         if (now < global.db.data.chats[m.chat].expired) global.db.data.chats[m.chat].expired += jumlahHari
         else global.db.data.chats[m.chat].expired = now + jumlahHari
+        conn.reply(res, `*${conn.user.name}* adalah bot whatsapp, diundang oleh @${m.sender.split`@`[0]}\n${conn.user.name} akan keluar otomatis setelah: ${await conn.msToDate(global.db.data.chats[res].expired - now)}`.trim(), null, { mentions: [m.sender], contextInfo: { mentionedJid: [m.sender] } })
     })
-    conn.reply(res, `*${conn.user.name}* adalah bot whatsapp, diundang oleh @${m.sender.split`@`[0]}\n${conn.user.name} akan keluar otomatis setelah: ${await conn.msToDate(global.db.data.chats[res].expired - now)}`.trim(), null, { mentions: [m.sender], contextInfo: { mentionedJid: [m.sender] } })
 }
 handler.help = ['join <chat.whatsapp.com>']
 handler.tags = ['premium']
