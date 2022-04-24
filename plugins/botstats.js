8bce30ed34c9e71d94555caf4496a30e466ba01b
FokusDotId <mr.familia13@gmail.com>
2022-04-24 19:02:28 +0700

    merapikan

--- plugins/botstats.js
+++ plugins/botstats.js
@@ -15,7 +15,7 @@ let handler = async (m, { conn }) => {
 ├ *${groups.length}* Grup
 ├ *${chats.length - groups.length}* Chat Pribadi
 ├ *${Object.keys(global.db.data.users).length}* Pengguna
-├ *${block == undefined ? '0 Diblokir' : block.length + ' Diblokir'}*
+├ ${block == undefined ? '*0* Diblokir' : '*' + block.length + '* Diblokir'}
 ├ *${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length}* Chat Terbanned
 ├ *${Object.entries(global.db.data.users).filter(user => user[1].banned).length}* Pengguna Terbanned
 └────
