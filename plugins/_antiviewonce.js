d311fa470af44ccf8563dcd35d23c636ac9771ce
FokusDotId <mr.familia13@gmail.com>
2022-04-25 21:58:38 +0700

    add antiviewonce

--- plugins/_antiviewonce.js
+++ plugins/_antiviewonce.js
@@ -1,12 +1,23 @@
+const { downloadContentFromMessage } = require('@adiwajshing/baileys')
 let handler = m => m
 
 handler.before = async function (m) {
     let chat = db.data.chats[m.chat]
     if (/^[.~#/\$,](read)?viewonce/.test(m.text)) return
     if (!chat.viewonce || chat.isBanned) return
-    let q = m.quoted ? m.quoted : m
-    if (q.mtype == 'viewOnceMessage') {
-        await this.copyNForward(m.chat, await this.loadMessage(m.chat, q.id), false, { readViewOnce: true })
+    if (m.mtype == 'viewOnceMessage') {
+        let msg = m.message.viewOnceMessage.message
+        let type = Object.keys(msg)[0]
+        let media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : 'video')
+        let buffer = Buffer.from([])
+        for await (const chunk of media) {
+            buffer = Buffer.concat([buffer, chunk])
+        }
+        if (/video/.test(type)) {
+            return this.sendFile(m.chat, buffer, 'media.mp4', msg[type].caption || '', m)
+        } else if (/image/.test(type)) {
+            return this.sendFile(m.chat, buffer, 'media.jpg', msg[type].caption || '', m)
+        }
     }
 }
 
