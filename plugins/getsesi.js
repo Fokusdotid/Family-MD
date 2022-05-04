aad453873b5b269cd1b7731ef8288c75b2c3dbf0
M imam Adi <84767049+Adi-OfficialL@users.noreply.github.com>
2022-05-04 13:33:51 +0700

    Delay is not defined🗿

--- plugins/getsesi.js
+++ plugins/getsesi.js
@@ -1,6 +1,5 @@
 let handler = async (m, { conn, text }) => {
     m.reply('Tunggu Sebentar, Proses Getting File session.data.json')
-    await delay(500)
     let sesi = await fs.readFileSync('./session.data.json')
     return await conn.sendMessage(m.chat, { document: sesi, mimetype: 'application/json', fileName: 'session.data.json' }, { quoted: m })
 }
