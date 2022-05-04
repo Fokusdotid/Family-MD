dd088e451885184d2c019354da2234506bef6e7d
Fokus ID <40955113+Fokusdotid@users.noreply.github.com>
2022-05-04 11:51:40 +0700

    Update uptime.js

--- plugins/uptime.js
+++ plugins/uptime.js
@@ -1,7 +1,7 @@
-let handler = async (m, { usedPrefix, command }) => {
+let handler = async (m, { conn, usedPrefix, command }) => {
     let _uptime = process.uptime() * 1000
     let uptimex = clockString(_uptime)
-    m.reply(uptimex)
+    conn.send3Button(m.chat, `Aktif selama: *${uptimex}*`.trim(), wm, 'bot stats', usedPrefix + 'stat', 'pinger', usedPrefix + 'ping', 'kembali ke menu', usedPrefix + 'menu', m)
 }
 handler.help = ['runtime']
 handler.tags = ['info']
@@ -10,8 +10,9 @@ handler.command = /^(uptime|runtime)$/i
 module.exports = handler
 
 function clockString(ms) {
+    let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
     let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
     let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
     let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-    return ['*' + h + ' Hour* ', '*' + m + ' Minute* ', '*' + s + ' Second*'].map(v => v.toString().padStart(2, 0)).join(' ')
+    return [d + ' Hari', + h + ' Jam ', + m + ' Menit ', s + ' Detik'].map(v => v.toString().padStart(2, 0)).join(' ')
 }
