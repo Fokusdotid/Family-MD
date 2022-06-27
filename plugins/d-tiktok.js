f2e7dc8c76749ea1bfeccf39c8b1563c3767e6ee
FokusDotId <mr.familia13@gmail.com>
2022-06-27 16:18:49 +0700

    update tiktok dl

--- plugins/d-tiktok.js
+++ plugins/d-tiktok.js
@@ -1,17 +1,15 @@
-const { tiktokdl } = require('@bochilteam/scraper')
+let axios = require('axios');
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-    const { author: { nickname }, video, description } = await tiktokdl(args[0])
-    const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
-    if (!url) throw 'Can\'t download video!'
-    conn.sendFile(m.chat, url, 'tiktok.mp4', `
-*Nickname:* ${nickname}
-*Description:* ${description}
-`.trim(), m)
+    if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+    let res = (await axios.get(API('males', '/tiktok', { url: args[0] } ))).data;
+    if (res.status != 200) throw res.message;
+    if (!res) throw res.message;
+    conn.sendButtonVid(m.chat, res.video, `*Judul:* ${res.title}\n${res.author ? `*Pembuat Video:* ${res.author}` : '\n' }`.trim(), wm, menu, usedPrefix + 'menu', m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-
 handler.command = /^(tik(tok)?(dl)?)$/i
+
+handler.limit = 1
 
 module.exports = handler
