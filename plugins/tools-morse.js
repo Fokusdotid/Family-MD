e955df9629755584193e9cf38f52bdb22479837b
nextamir <102469398+nextamir@users.noreply.github.com>
2022-07-29 20:02:53 +0430

    Add files via upload

---
+++ plugins/tools-morse.js
@@ -0,0 +1,136 @@
+let handler = async(m, { usedPrefix, command, text, isAdmin }) => {
+  if (!m.isGroup) return
+  if (global.db.data.chats[m.chat].onlyadmin === true && !isAdmin) return
+
+  let enc = {
+    "a": "•-",
+    "b": "-•••",
+    "c": "-•-•",
+    "d": "-••",
+    "e": "•",
+    "f": "••-•",
+    "g": "--•",
+    "h": "••••",
+    "i": "••",
+    "j": "•---",
+    "k": "-•-",
+    "l": "•-••",
+    "m": "--",
+    "n": "-•",
+    "o": "---",
+    "p": "•--•",
+    "q": "--•-",
+    "r": "•-•",
+    "s": "•••",
+    "t": "-",
+    "u": "••-",
+    "v": "•••-",
+    "w": "•--",
+    "x": "-••-",
+    "y": "-•--",
+    "z": "--••",
+    "0": "-----",
+    "1": "•----",
+    "2": "••---",
+    "3": "•••--",
+    "4": "••••-",
+    "5": "•••••",
+    "6": "-••••",
+    "7": "--•••",
+    "8": "---••",
+    "9": "----•",
+    "?": "••--••",
+    "!": "-•-•--",
+    ".": "•-•-•-",
+    ",": "--••--",
+    ";": "-•-•-•",
+    ":": "---•••",
+    "+": "•-•-•",
+    "-": "-••••-",
+    "/": "-••-•",
+    "=": "-•••-",
+    " ": "/"
+  }
+  let dec = {
+    "-----": "0",
+    "•----": "1",
+    "••---": "2",
+    "•••--": "3",
+    "••••-": "4",
+    "•••••": "5",
+    "-••••": "6",
+    "--•••": "7",
+    "---••": "8",
+    "----•": "9",
+    "•-": "a",
+    "-•••": "b",
+    "-•-•": "c",
+    "-••": "d",
+    "•": "e",
+    "••-•": "f",
+    "--•": "g",
+    "••••": "h",
+    "••": "i",
+    "•---": "j",
+    "-•-": "k",
+    "•-••": "l",
+    "--": "m",
+    "-•": "n",
+    "---": "o",
+    "•--•": "p",
+    "--•-": "q",
+    "•-•": "r",
+    "•••": "s",
+    "-": "t",
+    "••-": "u",
+    "•••-": "v",
+    "•--": "w",
+    "-••-": "x",
+    "-•--": "y",
+    "--••": "z",
+    "••--••": "?",
+    "-•-•--": "!",
+    "•-•-•-": ".",
+    "--••--": ",",
+    "-•-•-•": ";",
+    "---•••": ":",
+    "•-•-•": "+",
+    "-••••-": "-",
+    "-••-•": "/",
+    "-•••-": "=",
+    "/": " "
+  }
+
+  let selected = text.toLowerCase().split(" ")[0] + " "
+  if(selected == "encode ") {
+    let str = text.replace(selected, "").toLowerCase()
+    let Output_Morse = ""
+    for(let i of str) {
+      if(!enc[i]) Output_Morse += i
+      for(let j in enc) {
+        if(j == i) Output_Morse += enc[i] + " "
+      }
+    }
+
+    m.reply(Output_Morse)
+  } else if(selected == "decode ") {
+    let str = text.replace(selected, "").replace(/[.]/g, "•")
+    let Output_String = ""
+    for(let i of str.split(" ")) {
+      if(!dec[i]) Output_String += i
+      for(let j in dec) {
+        if(j == i) Output_String += dec[i]
+      }
+    }
+
+    m.reply(Output_String)
+  } else {
+    if (global.db.data.chats[m.chat].englishlang === true) m.reply(`*_insert text!_*\n_For example:_\n\n*${usedPrefix}${command} encode Hello World*\n*${usedPrefix}${command} decode •••• • •-•• •-•• --- / •-- --- •-• •-•• -••*`)
+    else m.reply(`*متن را به درستی وارد کنید!*\nبرای مثال :\n\n*${usedPrefix}${command} encode Hello World*\n*${usedPrefix}${command} decode •••• • •-•• •-•• --- / •-- --- •-• •-•• -••*`)
+  }
+}
+handler.help = ["morse encode [text]", "morse decode [morse]"]
+handler.tags = ["tools"]
+handler.command = /^(morse)/i
+
+module.exports = handler
