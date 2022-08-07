3ea9a57fa28264a9c41342c36095a5198f08b6e4
Alvinocaharles <54900965+alvino-prog@users.noreply.github.com>
2022-08-07 06:27:43 +0000

    Update d-tiktok.js

--- plugins/d-tiktok.js
+++ plugins/d-tiktok.js
@@ -1,15 +1,38 @@
-let axios = require('axios');
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-    let res = (await axios.get(API('males', '/tiktok', { url: args[0] } ))).data;
-    if (res.status != 200) throw res.message;
-    if (!res) throw res.message;
-    conn.sendButtonVid(m.chat, res.video, `*Judul:* ${res.title}\n${res.author ? `*Pembuat Video:* ${res.author}` : '\n' }`.trim(), 'Cara simpan digalery:\n1. Download dulu videonya\n2. Buka terus klik titik 3 pojok kanan atas\n3. lalu klik simpan!', 'menu', usedPrefix + 'menu', m)
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+  if (!args[0]) throw 'Uhm...url nya mana?'
+  let res = await fetch(global.API('xteam', '/dl/tiktok', {
+    url: args[0]
+  }, 'APIKEY'))
+  if (res.status !== 200) throw await res.text()
+  let json = await res.json()
+  if (!json.status) throw json
+  /*let url = json.server_1 || json.info[0].videoUrl || ''
+  if (!url) throw 'Gagal mengambil url download'
+  let txt = json.info[0].text
+  for (let hashtag of json.info[0].hashtags) txt = txt.replace(hashtag, '*$&*')
+  await conn.sendFile(m.chat, url, 'tiktok.mp4', `
+▶ ${json.info[0].playCount} Views
+❤ ${json.info[0].diggCount} Likes
+🔁 ${json.info[0].shareCount} Shares
+💬 ${json.info[0].commentCount} Comments
+🎵 ${json.info[0].musicMeta.musicName} by ${json.info[0].musicMeta.musicAuthor}
+- *By:* ${json.info[0].authorMeta.nickName} (${json.info[0].authorMeta.name})
+- *Desc:*
+${txt}
+  `.trim(), m)*/
+  let url = json.result.link_dl1 || json.result.link_dl2 || ''
+  if (!url) throw 'Gagal mengambil url download'
+  let txt = `
+  - *By:* ${json.result.name}
+  - *Caption:*
+  ${json.result.caption}
+    `
+    await conn.sendFile(m.chat, url, 'tiktok.mp4', txt.trim(), m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(tik(tok)?(dl)?)$/i
 
-handler.limit = 1
+handler.command = /^(tik(tok)?(dl)?)$/i
 
 module.exports = handler
