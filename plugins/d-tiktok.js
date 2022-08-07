6842e44761c0e909aa10a5c4e70154a124e65f60
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-08-07 14:35:35 +0700

    Update d-tiktok.js

--- plugins/d-tiktok.js
+++ plugins/d-tiktok.js
@@ -1,7 +1,7 @@
 let axios = require('axios');
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-    let res = (await axios.get(API('males', '/tiktok', { url: args[0] } ))).data;
+    let res = (await axios.get(API('males', '/downloader/tiktok', { url: args[0] } ))).data;
     if (res.status != 200) throw res.message;
     if (!res) throw res.message;
     conn.sendButtonVid(m.chat, res.video, `*Judul:* ${res.title}\n${res.author ? `*Pembuat Video:* ${res.author}` : '\n' }`.trim(), 'Cara simpan digalery:\n1. Download dulu videonya\n2. Buka terus klik titik 3 pojok kanan atas\n3. lalu klik simpan!', 'menu', usedPrefix + 'menu', m)
