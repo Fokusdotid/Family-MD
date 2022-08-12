e0c34017213a7b5895a5028a230bc5854df2ef74
Alvinocaharles <54900965+alvino-prog@users.noreply.github.com>
2022-08-12 16:57:24 +0000

    Update internet-charainfo.js

--- plugins/internet-charainfo.js
+++ plugins/internet-charainfo.js
@@ -1,16 +1,18 @@
-let fetch = require('node-fetch');
+let fetch = require('node-fetch')
 let handler = async(m, { conn, text }) => {
   if (!text) throw `Masukkan query!`
-  let res = await fetch(global.API('https://api.jikan.moe', '/v3/search/character', { q: text }))
+  let res = await fetch(global.API('https://api.jikan.moe', '/v4/characters', { q: text }))
   if (!res.ok) throw await res.text()
   let json = await res.json()
-  let { name, alternative_names, url, image_url, type } = json.results[0]
-  let charaingfo = `💬 *Name:* ${name}
-💭 *Nickname:* ${alternative_names}
+  console.log(json)
+  let { name, nicknames, name_kanji, url, image_url, about, type } = json.data[0]
+let charaingfo = `💬 *Name:* ${name} ${name_kanji}
+💭 *Nickname:* ${nicknames}
 🔗 *Link*: ${url}
-👤 *Character Type*: ${type}`
+👤 *About*: ${about}`
 
   conn.sendFile(m.chat, image_url, '', charaingfo, m)
+  // fix kan gus, ga bisa send gambar, malah jadi .bin file
 }
 handler.help = ['character <nama>']
 handler.tags = ['internet']
